f5ebde29e7f6b9aa92ec4b1f0f5b53ff851e4ef2
johnenes <johneneojo1@gmail.com>
2023-06-01 10:01:02 -0700

    Accepting input from the shell command

---
+++ ShellCommand/bashspace/acceptInput.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+echo " what is your name?"
+
+read name
+
+echo "I came visiting you at school today, i could not found you. where do you went druing your break hour $name ?"
+
+sleep 2
+
+echo "$name I told you serveral time to stop moving around durng the break period"
+
+sleep 2
+
+echo "I hope you did well your assignmant $name"
+
+sleep 1
+
+echo "Awesome!! $anme"
