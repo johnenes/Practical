1c48c4acc54cdfc3d79f568159cf7f8d21ec13cc
johnenes <johneneojo1@gmail.com>
2023-06-01 09:44:05 -0700

    Static script

---
+++ ShellCommand/bashspace/staticScript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+name="Henry" 
+
+echo "Hello $name it great day to see you again"
+sleep 1
+echo "You look every young evertime i see you $name"
+sleep 1
+echo  "Great to met you again see you some other time $name"
